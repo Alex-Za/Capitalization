26bb9e03e1c02c9ab32e32833258311d5a0c0d61
Alex-Za <38041377+Alex-Za@users.noreply.github.com>
2020-09-01 22:25:50 +0300

    from home

--- Capitalization/Classes/Processing.cs
+++ Capitalization/Classes/Processing.cs
@@ -32,7 +32,7 @@ namespace Capitalization.Classes
         {
             //удалить колонки: первую,
             string[] columnsToDelete = {"Internal type of work", "D Hours", "M Hours", "In Ratio Hours",
-                "Total time(h)", "D Cost", "M Cost", "In Ratio Cost", "Total cost", "inFileName" }; //так же удалить первую колонку
+                "Total time (h)", "D Cost", "M Cost", "In Ratio Cost", "Total cost", "inFileName" }; //так же удалить первую колонку
             masterFile = new DataTable();
             masterFile.TableName = "Sheet1";
             string[] columnNames = fileReader.CapitalizationFile.Columns.Cast<DataColumn>().
@@ -46,9 +46,23 @@ namespace Capitalization.Classes
                 masterFile.Rows.Add(row.ItemArray);
             }
 
+            masterFile.Columns.RemoveAt(0);
+            masterFile.Columns.RemoveAt(masterFile.Columns.Count-1);
             foreach (var name in columnsToDelete)
                 if (masterFile.Columns.Contains(name))
                     masterFile.Columns.Remove(name);
+
+            //int lastRow = masterFile.Rows.Count - 1;
+            //int timeHIndex = masterFile.Columns["Time (h)"].Ordinal+1;
+            //double newBrands = (double)masterFile.Rows[lastRow - 2][timeHIndex];
+            //double addtoEx = (double)masterFile.Rows[lastRow - 2].ItemArray[timeHIndex];
+            //double workRel = (double)masterFile.Rows[lastRow-1].ItemArray[timeHIndex];
+            //double workPRel = (double)masterFile.Rows[lastRow].ItemArray[timeHIndex];
+            //double totalPlanfix = workPRel + workRel + addtoEx + newBrands;
+
+            //DataRow newRow = masterFile.Rows.Add();
+            //newRow[timeHIndex] = totalPlanfix;
+            //newRow[1] = "Total Planfix";
 
         }
 
@@ -57,10 +71,10 @@ namespace Capitalization.Classes
             using (var workbook = new XLWorkbook())
             {
                 workbook.Worksheets.Add(MasterFile);
-                workbook.Worksheet(1).Outline.SummaryVLocation = XLOutlineSummaryVLocation.Top;
-                workbook.Worksheet(1).Rows(3, 833).Group();
-                workbook.Worksheet(1).Rows(4, 17).Group();
-                workbook.Worksheet(1).CollapseRows();
+                //workbook.Worksheet(1).Outline.SummaryVLocation = XLOutlineSummaryVLocation.Top;
+                //workbook.Worksheet(1).Rows(3, 833).Group();
+                //workbook.Worksheet(1).Rows(4, 17).Group();
+                //workbook.Worksheet(1).CollapseRows();
 
                 workbook.SaveAs("New.xlsx");
             }
